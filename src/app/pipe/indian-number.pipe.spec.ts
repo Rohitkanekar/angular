fa58fc9af55a840f9a3dcfd3978ac902b119be0d
rohit <kanekar.rohit@avionsoftware.in>
2025-09-04 18:28:57 +0530

    Remove all files from repo

--- src/app/pipe/indian-number.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IndianNumberPipe } from './indian-number.pipe';
-
-describe('IndianNumberPipe', () => {
-  it('create an instance', () => {
-    const pipe = new IndianNumberPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
